bba655886a470c9a395a2c0dacf04624a8d687fa
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2023-09-11 17:11:41 +0300

    task_1 for lab_1 added

---
+++ main.py
@@ -0,0 +1,12 @@
+from lab_1.task_1 import simple_event_simulator
+
+PROBABILITY = 0.6
+
+
+def main():
+    print("-"*10 + "Task 1" + "-"*10)
+    simple_event_simulator(PROBABILITY)
+
+
+if __name__ == "__main__":
+    main()
